637290b66043332025ee5dcd172822ef6d881b0f
TobiasUr <68461330+TobiasUr@users.noreply.github.com>
2022-10-08 18:59:22 +0200

    Added progress bar

--- Main.py
+++ Main.py
@@ -2,7 +2,10 @@
 from importlib.resources import path
 from tkinter import *
 from tkinter import filedialog
+from tkinter.ttk import Progressbar
 from pytube import YouTube
+from pytube.cli import on_progress
+import threading
 
 root = Tk()
 #VideoLinkEntry
@@ -16,7 +19,9 @@ def selectPath():
     Label(root, text=path).grid(row=1, column=1, sticky='w')
 
 Button(root, text="Select path", command=selectPath).grid(row=1, column=1, sticky='e')
-
+#progress bar
+pb = Progressbar(root, orient='horizontal', mode='determinate',length=320)
+pb.grid(row=2, column=1)
 
 #DropdownMenu
 OPTIONS = [
@@ -31,12 +36,22 @@ variable.set(OPTIONS[2])
 QualitySelect = OptionMenu(root, variable, *OPTIONS)
 QualitySelect.grid(row=2, column=1, sticky='w')
 
+#update on download progress
+def on_progress(stream, chunk, bytes_remaining):
+    size = stream.filesize
+    progress = (size - bytes_remaining)/size
+    pb['value'] = progress*100
+    root.update_idletasks()  
+    print(progress*100)
+
+
 #downloads
 def download():
     Link = LinkEntry.get()
     global path
     Path=path
-    yt = YouTube(Link)
+    yt = YouTube(Link, on_progress_callback=on_progress)
+    
     
     if variable.get() == "High":
         yt = yt.streams.get_highest_resolution()
@@ -51,8 +66,10 @@ def download():
     if variable.get() == "Audio":
         yt = yt.streams.get_audio_only()
         yt.download(Path)
-
-Button(root, text="Download", command=download).grid(row=2, column=0)
+def start():
+    x = threading.Thread(target=download)
+    x.start()
+Button(root, text="Download", command=start).grid(row=2, column=0)
 
 
 
